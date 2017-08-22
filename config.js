ffd9abbe2ac586a2e6501bd3eb91ce5bc42fb76b
Eric Laursen <ericjlaursen@gmail.com>
2017-08-22 06:50:46 -0400

    reduces DB url to mLab only

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/trips';
+                      // 'mongodb://localhost/trips';
                       'mongodb://tripTaker:takingTrips247@ds023463.mlab.com:23463/loggedtrips';
 exports.PORT = process.env.PORT || 8080;
