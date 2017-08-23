b9240ee92afc59bf58a416ac95ac014b6f38c51f
Eric Laursen <ericjlaursen@gmail.com>
2017-08-23 13:18:01 -0400

    includes test-database-url

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      // 'mongodb://localhost/trips';
-                      'mongodb://tripTaker:takingTrips247@ds023463.mlab.com:23463/loggedtrips';
+                      'mongodb://localhost/trips';
+                      // 'mongodb://tripTaker:takingTrips247@ds023463.mlab.com:23463/loggedtrips';
+exports.TEST_DATABASE_URL = (
+	process.env.TEST_DATABASE_URL ||
+	'mongodb://localhost/trips');
 exports.PORT = process.env.PORT || 8080;
