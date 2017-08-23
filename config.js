3967b2a546b975925dc71e51eeefb04dc486b47b
Eric Laursen <ericjlaursen@gmail.com>
2017-08-23 13:27:41 -0400

    uses mLab url

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/trips';
-                      // 'mongodb://tripTaker:takingTrips247@ds023463.mlab.com:23463/loggedtrips';
+                      // 'mongodb://localhost/trips';
+                      'mongodb://tripTaker:takingTrips247@ds023463.mlab.com:23463/loggedtrips';
 exports.TEST_DATABASE_URL = (
 	process.env.TEST_DATABASE_URL ||
 	'mongodb://localhost/trips');
