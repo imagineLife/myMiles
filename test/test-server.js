04ddd459c545d480c8947c56dba41fb57cc6516c
Eric Laursen <ericjlaursen@gmail.com>
2017-08-23 18:29:24 -0400

    adds successful local GET test

--- test/test-server.js
+++ test/test-server.js
@@ -10,7 +10,7 @@ const should = chai.should();
 
 chai.use(chaiHttp);
 
-describe('Trip API resources', () => {
+describe('Trips API resources page \n', () => {
 
 	before(function() {
 		return runServer(TEST_DATABASE_URL);
@@ -20,14 +20,34 @@ describe('Trip API resources', () => {
 		return closeServer();
 	})
 
-	it('index page should show up', (done) => {
-		chai.request(app)
-		.get('/')
-		.end((err, resp) => {
-			resp.should.have.status(200);
-			resp.should.be.html;
-			done();
-		})
-	})
+	describe('GET endpoint', function() {
+
+		it('should return all existing Trips', function() {
+			// strategy:
+			//    1. get back all Trips returned by by GET request to `/trips`
+			//    2. prove res has right status, data type
+			//    3. prove the number of Trips we got back is equal to number
+			//       in db.
+			//
+			// need to have access to mutate and access `res` across
+			// `.then()` calls below, so declare it here so can modify in place
+			let res;
+			return chai.request(app)
+				.get('/trips')
+				.then(function(_res) {
+				  // res, so that subsequent (.then) blocks can access /trips response obj (_res).
+				  res = _res;
+				  res.should.have.status(200);
+				  // otherwise our db seeding didn't work
+				  res.body.should.have.length.of.at.least(1);
+				  return Trip.count();
+				})
+				.then(function(count) {
+					// res.body.should.have.length.of(count);
+					res.body.length.should.equal(count);
+				});
+    	});
+    });
 
 });
+
