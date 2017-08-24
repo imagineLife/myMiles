ae1546a54bcbbfa18215ef62f97a2a159d68e964
Eric Laursen <ericjlaursen@gmail.com>
2017-08-23 21:32:08 -0400

    implements fn to build table rows

--- public/index.js
+++ public/index.js
@@ -4,15 +4,30 @@ function getResFromAPI() {
 	const infoSettings = {
       url: getTripURI,  
       dataType: 'json',
-      success: displayAPISearchData,
+      success: generateTableRows,
       error: function(err) { console.log(err) },
     };
 
     $.ajax(infoSettings);
 }
 
-function displayAPISearchData(data){
-	console.log(data);
+function generateTableRows(data){
+	let rowsHTML = '';
+
+
+	for(let i=0; i<data.length; i++){
+		const currentData = data[i];
+		const rowHTML = (
+			`<tr>
+	            <td>${currentData.date}</td>
+	            <td>${currentData.milesTraveled}</td>
+	          </tr>`
+	          );
+
+		rowsHTML = rowsHTML + rowHTML;
+	}
+
+	console.log(rowsHTML);
 }
 
 function doEverything(){
@@ -22,3 +37,6 @@ function doEverything(){
 }
 
 $(doEverything);
+
+
+          
