fe6e97ffc73eb7ebc59e97691f954799db0f1a27
Eric Laursen <ericjlaursen@gmail.com>
2017-08-25 16:39:03 -0400

    includes DELETE endpoint

--- server.js
+++ server.js
@@ -49,6 +49,41 @@ app.post('/trips', (req, res) => {
 
 });
 
+app.put('/trips/:id', (req, res) => {
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    res.status(400).json({
+      error: 'Request path id and request body id values must match'
+    });
+  }
+
+  const updated = {};
+  const updateableFields = ['milesTraveled', 'date'];
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      updated[field] = req.body[field];
+    }
+  });
+
+  Trip
+    .findByIdAndUpdate(req.params.id, {$set: updated}, {new: true})
+    .exec()
+    .then(updatedPost => res.status(201).json(updatedPost.apiRepr()))
+    .catch(err => res.status(500).json({message: 'Something went wrong'}));
+});
+
+app.delete('/trips/:id', (req, res) => {
+  Trip
+    .findByIdAndRemove(req.params.id)
+    .exec()
+    .then(() => {
+      res.status(204).json({message: 'success'});
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({error: 'something went terribly wrong'});
+    });
+});
+
 app.use('*', function(req, res) {
   res.status(404).json({message: 'Not Found'});
 });
