1be610737bcb85cbbddda65bc4dc02d7cdb10136
Eric Laursen <ericjlaursen@gmail.com>
2017-08-29 12:28:23 -0400

    collects form input vals

--- public/js/add.js
+++ public/js/add.js
@@ -1,5 +1,15 @@
+//Gathers form input values on Submit-Click
+//and passes values to a validation function (checkInputs)
 $("#form")
-	.on('click', '#submit', () =>{
-		console.log('testing click spot!');
+	.on('click', '#submit', (ev) =>{
+		ev.preventDefault();
 
+		let inputList = $('#form input');
+		let inputVals = {};
+		for (i = 0; i < inputList.length; i++){
+			inputVals[i] = inputList[i].value;
+		}
+		console.log('inputVals are ', inputVals);
+
+	//checkInputs(inputVals);
 	})
