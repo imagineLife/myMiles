09b837045edc0d461ba469c09bcfb0c9f0e73727
Eric Laursen <ericjlaursen@gmail.com>
2017-08-29 15:29:44 -0400

    gathers and checks for correct input entry count

--- public/js/add.js
+++ public/js/add.js
@@ -1,15 +1,27 @@
+function validateCorrectInputCount(inputVals){
+    if(Object.keys(inputVals).length == 2){
+    	console.log('enough inputs!!');
+    	// parseFormInputs();
+    }else{
+    	console.log('not enought inputs');
+    }
+}
+
 //Gathers form input values on Submit-Click
 //and passes values to a validation function (checkInputs)
 $("#form")
 	.on('click', '#submit', (ev) =>{
 		ev.preventDefault();
 
-		let inputList = $('#form input');
-		let inputVals = {};
-		for (i = 0; i < inputList.length; i++){
-			inputVals[i] = inputList[i].value;
-		}
-		console.log('inputVals are ', inputVals);
+	    let inputList = $("#form input");
+	    let inputVals = {};
+	    
+	    for (i = 0; i < inputList.length; i++){
+	    	if(inputList[i].value == ''){}else{
+	      		inputVals[i] = inputList[i].value;
+	      	}
+	    };
 
-	//checkInputs(inputVals);
+	    // validateCorrectInputCount(inputVals);
+
 	})
