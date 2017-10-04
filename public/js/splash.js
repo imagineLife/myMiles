a2468afc472cfe65599e512f67ffd51640178c42
Eric Laursen <ericjlaursen@gmail.com>
2017-10-04 18:22:37 -0400

    sets demo-login on splash-screen, removes from login screen

--- public/js/splash.js
+++ public/js/splash.js
@@ -9,3 +9,36 @@ $(function(){
       .attr('href', '/trips');
   }
 })
+
+function redirect(user){
+	Cookies.set('authToken', user.authToken);
+	console.log(Cookies.get('authToken'));
+	window.location='/trips'
+}
+
+function logInDemo(){
+	let loginURL = '/api/auth/login';
+	let demoCredentials = {username: "demoUser", password: "DemoUserPassword"};
+	const { username, password } = demoCredentials;
+	const encodedStr = btoa(`${username}:${password}`);
+
+	const infoSettings = {
+	    url: loginURL,
+	    type: 'POST',
+		beforeSend: req => {
+		    req.setRequestHeader('Authorization', 'Basic ' + encodedStr);
+		  },
+  	    contentType: 'application/json',
+	    data: JSON.stringify(demoCredentials),
+	};
+
+	$.ajax(infoSettings)
+		.then(redirect)
+		.catch((err)=>console.log(err));
+}
+
+$('#demoButton')
+	.on('click', (ev)=>{
+		ev.preventDefault();
+		logInDemo();
+	})
