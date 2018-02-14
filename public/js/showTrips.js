8bb9536d363c2e7f868dd0a1235a809e66c0d3e5
Eric Laursen <ericjlaursen@gmail.com>
2018-02-14 05:49:51 -0500

    Includes function to show and hide inputs on click.

--- public/js/showTrips.js
+++ public/js/showTrips.js
@@ -24,12 +24,19 @@ function generateTableHTML(data){
 
 	for(let i=0; i<data.length; i++){
 		const currentData = data[i];
+		// console.log('currentData ->',currentData);
 		let parsedDate = currentData.date.slice(0, 10);
 
 		const rowHTML = (
 			`<tr>
 	            <td>${parsedDate}</td>
-	            <td>${currentData.milesTraveled}</td>
+	            <td>
+	            	<span class="mileValue" name="mileValue" data-id="${currentData._id}" value="${currentData.milesTraveled}">
+	            		${currentData.milesTraveled}
+	            	</span>
+	            	<button class="submitCheckbox">√</button>
+	            	<button class="cancelX">X</button>
+	            </td>
 	          </tr>`
 	          );
 
@@ -38,6 +45,56 @@ function generateTableHTML(data){
 	}
 
 	displayTable($('.trip-table-body'), rowsHTML, $('.trip-table-total-miles'), totalMilesTraveled);
+}
+
+//editable mileage
+$('.trip-table-body')
+	.on('click','span', function(e){
+
+		//add the closeInput
+		closeInput();	
+		/*convert to input
+			traverse up a level to the table
+			convert span WORD to input
+		*/
+
+		//remove current string
+		$(this).html('');
+
+		//make input out of span
+		$(this).closest('td').html(
+			$(this).closest('td').html().replace(/span/g,'input')
+		)
+
+	})
+
+		/*
+			add check-mark submit
+			add x-cancel
+			add listeners to these
+			make input-to-span conversion for after editing cell
+			disable multiple inputs @ same time 
+		*/
+
+//converts an open input to a span
+function closeInput(){
+	$('input.mileValue')
+		.each(function(){
+
+		//pull original value out of element
+			let origVal = $(this)[0].value;
+			let tData = $(this).closest('td');
+
+		//reset input back to span
+			$(tData).html(
+				$(tData).html().replace(/input/g,'span')
+			)
+		
+		//set span value back to value
+			$(tData).find('span').text(origVal);
+
+		})
+
 }
 
 function displayTable(tBodyElem, rowsHTML, tTotalElem, totMiles){
