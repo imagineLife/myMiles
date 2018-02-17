0be247543d220b8887fbbfb204baad3ee65ed9c5
Eric Laursen <ericjlaursen@gmail.com>
2018-02-16 20:02:43 -0500

    Extends expiration from 5m to 10m.

--- config.js
+++ config.js
@@ -6,4 +6,4 @@ exports.TEST_DATABASE_URL = (
 	'mongodb://localhost/trips');
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
-exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '5m';
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '10m';
